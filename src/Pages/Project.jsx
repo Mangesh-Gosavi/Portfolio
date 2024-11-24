e78f55ed6be17b02d25673f4fa317d3ab58352da
Mangesh Namdeo Gosavi <148337560+Mangesh-Gosavi@users.noreply.github.com>
2024-11-24 12:41:37 +0530

    Update Project.jsx

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -1,8 +1,10 @@
-import img from '../assets/skill.png';
 import proj1 from '../assets/supchain.jpg';
 import proj2 from '../assets/shop.jpg';
 import proj3 from '../assets/admin.jpg';
-import proj4 from '../assets/music.jpg';
+import proj4 from '../assets/weather.jpg';
+import proj5 from '../assets/auction.jpg';
+import proj6 from '../assets/recepie.jpg';
+import proj7 from '../assets/blog.jpg';
 import { Link } from 'react-router-dom';
 
 
@@ -30,18 +32,23 @@ function Project() {
           /></Link>
           <Link to=''><img
             className="w-60 h-60 md:w-96 md:h-96 rounded-lg shadow-lg hover:scale-110 transition duration-1000"
+            src={proj5}
+            alt="Project 4 Loading"
+          /></Link>
+          <Link to=''><img
+            className="w-60 h-60 md:w-96 md:h-96 rounded-lg shadow-lg hover:scale-110 transition duration-1000"
+            src={proj7}
+            alt="Project 7 Loading"
+          /></Link>
+          <Link to=''><img
+            className="w-60 h-60 md:w-96 md:h-96 rounded-lg shadow-lg hover:scale-110 transition duration-1000"
+            src={proj6}
+            alt="Project 6 Loading"
+          /></Link>
+          <Link to='https://weatherforecast-rosy-chi.vercel.app/'><img
+            className="w-60 h-60 md:w-96 md:h-96 rounded-lg shadow-lg hover:scale-110 transition duration-1000"
             src={proj4}
-            alt="Project 1 Loading"
-          /></Link>
-          <Link to='https://pooja-collection-eosin-delta.vercel.app/'><img
-            className="w-60 h-60 md:w-96 md:h-96 rounded-lg shadow-lg hover:scale-110 transition duration-1000"
-            src={proj2}
-            alt="Project 2 Loading"
-          /></Link>
-          <Link to='https://pooja-collection-admin.vercel.app/'><img
-            className="w-60 h-60 md:w-96 md:h-96 rounded-lg shadow-lg hover:scale-110 transition duration-1000"
-            src={proj3}
-            alt="Project 3 Loading"
+            alt="Project 4 Loading"
           /></Link>
 
         </div>
