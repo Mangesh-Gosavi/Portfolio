b257f72cab1f08cd0464ee3f4261f13f797294ce
Mangesh Namdeo Gosavi <148337560+Mangesh-Gosavi@users.noreply.github.com>
2025-06-14 13:24:13 +0530

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,11 +2,10 @@ import img from '../assets/mangesh.jpeg';
 import resume from "../assets/MangeshGosavi.pdf";
 
 function About() {
-
   const onButtonClick = () => {
     const link = document.createElement("a");
-    link.href = resume; 
-    link.download = "MangeshGosavi_Resume.pdf"; 
+    link.href = resume;
+    link.download = "MangeshGosavi_Resume.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -20,24 +19,28 @@ function About() {
         src={img}
         alt="Mangesh Namdeo Gosavi"
       />
-      
+
       <div className="bg-white text-gray-900 p-6 rounded-lg shadow-lg shadow-slate-600 max-w-xl
       hover:scale-110 transition duration-1000">
         <h3 className="text-xl font-bold mb-4">
           Hi, I'm Mangesh Namdeo Gosavi
         </h3>
         <p className="text-sm leading-6">
-          I'm a full-stack developer and design enthusiast aiming to excel as a web or software developer. 
-          I feel comfortable working with technologies like HTML, CSS, JavaScript, React, Tailwind, Node.js, MySQL, 
-          Figma, and many others in the web ecosystem.
+          I'm a full-stack developer and design enthusiast aiming to excel as a web or software developer.
+          I work confidently with technologies like HTML, CSS, JavaScript, React, Tailwind, Node.js, MySQL, Figma, and other tools in the web ecosystem.
         </p>
         <p className="text-sm leading-6 mt-4">
-          I love programming, design, and user interfaces—my true passions. Beyond work, I was a national-level 
-          sports player in Handball. Currently, I'm working on a MERN project and diving into ML for automation.
+          I’m passionate about programming, user interfaces, and clean design. I'm also expanding my skillset in data analytics, exploring tools like Python, Pandas, Power BI, and Excel to derive insights and solve real-world problems through data.
+        </p>
+        <p className="text-sm leading-6 mt-4">
+          Outside of tech, I was a national-level handball player—an experience that taught me resilience and teamwork. Currently, I’m working on a MERN project and diving deeper into machine learning and automation.
         </p>
 
         <div className="mt-6">
-          <button  onClick={onButtonClick} className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow transition duration-300">
+          <button
+            onClick={onButtonClick}
+            className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow transition duration-300"
+          >
             Download CV
           </button>
         </div>
